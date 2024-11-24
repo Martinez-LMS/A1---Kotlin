0c438203d71693926f6c3d0e9d86246c5dc77fde
Martinez-LMS <leo.ooosla@gmail.com>
2024-11-23 21:26:20 -0300

    quase funcionando o salvar

--- basic-android-kotlin-compose-training-inventory-app-starter/app/src/main/java/com/example/inventory/ui/home/HomeScreen.kt
+++ basic-android-kotlin-compose-training-inventory-app-starter/app/src/main/java/com/example/inventory/ui/home/HomeScreen.kt
@@ -49,6 +49,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.inventory.InventoryTopAppBar
 import com.example.inventory.R
+import com.example.inventory.data.Item
 import com.example.inventory.ui.item.formatedPrice
 import com.example.inventory.ui.navigation.NavigationDestination
 import com.example.inventory.ui.theme.InventoryTheme
