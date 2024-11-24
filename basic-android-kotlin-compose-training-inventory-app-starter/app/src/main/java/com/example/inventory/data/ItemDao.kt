9ffa057a09624d43d97806ce5c594c9ac85bbb2c
Martinez-LMS <leo.ooosla@gmail.com>
2024-11-23 21:12:03 -0300

    Banco de dados

--- basic-android-kotlin-compose-training-inventory-app-starter/app/src/main/java/com/example/inventory/data/ItemDao.kt
+++ basic-android-kotlin-compose-training-inventory-app-starter/app/src/main/java/com/example/inventory/data/ItemDao.kt
@@ -1,11 +1,10 @@
-package com.example.inventory.data
-
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
+import com.example.inventory.data.Item
 import kotlinx.coroutines.flow.Flow
 
 @Dao
